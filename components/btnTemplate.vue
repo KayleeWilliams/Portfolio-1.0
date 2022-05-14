c01339d7eaf471ac9699e8dd42f3f2c4b2b53d59
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-05-14 02:34:44 +0100

    Updated buttons

--- components/btnTemplate.vue
+++ components/btnTemplate.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a
-      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md p-5 w-40"
+      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md py-3 px-5 w-40 ease-in-out duration-300"
       v-show="bType == 'Nav'"
       v-bind:href="bURL"
       v-on:click="$emit('gcGo', bDest)"
@@ -10,7 +10,7 @@
     </a>
 
     <a
-      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md p-5 w-40"
+      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md py-3 px-5 w-40 ease-in-out duration-300"
       v-show="bType == 'Show'"
       v-bind:href="bURL"
       v-on:click="$emit('buttonClick')"
@@ -19,7 +19,7 @@
     </a>
 
     <a
-      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md p-5 w-40"
+      class="text-base select-none bg-slateblue hover:bg-lavendergrey rounded-md shadow-md py-3 px-5 w-40 ease-in-out duration-300"
       v-show="bType == 'Other'"
       v-bind:href="bURL"
     >
