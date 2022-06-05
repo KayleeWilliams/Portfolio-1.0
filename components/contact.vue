abf9f33ecb5f1388d5d005051444749c9315d029
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-06-05 23:40:51 +0100

    Updated email, removed twitter and instagram

--- components/contact.vue
+++ components/contact.vue
@@ -2,6 +2,6 @@
     <div class="flex flex-col s:items-start md:items-center s:gap-4 md:gap-16">
         <p class="s:text-2xl md:text-4xl font-bold"> Contact Me! </p>
         <p class="s:text-sm s:max-w-xs md:text-2xl md:max-w-none"> I am currently looking for oppertunities.<br>Please feel free to reach out for a chat. 😃 </p>
-        <btnTemplate class="s:pt-4 md:pt-0" bType="Other" bURL="mailto:kayleew795@gmail.com" bText="Let's Chat!" />
+        <btnTemplate class="s:pt-4 md:pt-0" bType="Other" bURL="mailto:kaylee@kayleewilliams.dev" bText="Let's Chat!" />
     </div>
 </template>
--- components/socialbar.vue
+++ components/socialbar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col gap-2 items-center w-0">
         <div class="border-solid border border-white w-px h-12"></div>
-        <a href="#"> <i class="fa-brands fa-twitter w-6 h-6 text-slateblue hover:text-lavendergrey" /></a>
-        <a href="#"> <i class="fa-brands fa-instagram w-6 h-6 text-slateblue hover:text-lavendergrey" /></a>
+        <!-- <a href="#"> <i class="fa-brands fa-twitter w-6 h-6 text-slateblue hover:text-lavendergrey" /></a>
+        <a href="#"> <i class="fa-brands fa-instagram w-6 h-6 text-slateblue hover:text-lavendergrey" /></a> -->
         <a href="https://www.linkedin.com/in/kaylee-williams-1559251bb/"> <i class="fa-brands fa-linkedin-in w-6 h-6 text-slateblue hover:text-lavendergrey" /></a>
         <a href="https://github.com/KayleeWilliams"> <i class="fa-brands fa-github w-6 h-6 text-slateblue hover:text-lavendergrey" /></a>
     </div>
